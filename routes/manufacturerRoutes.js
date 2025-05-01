6fa65ae49da883df6062c988b967a103a3744e55
Ankit Tripathi <168020066+ankittrip@users.noreply.github.com>
2025-05-02 05:08:52 +0530

    Update manufacturerRoutes.js

--- routes/manufacturerRoutes.js
+++ routes/manufacturerRoutes.js
@@ -1,14 +1,13 @@
-// routes/manufacturerRoutes.js
+
 const express = require('express');
 const router = express.Router();
 const manufacturerController = require('../controllers/manufacturerController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-// Public routes
 router.get('/', manufacturerController.getAllManufacturers);
 router.get('/:id', manufacturerController.getManufacturerById);
 
-// Protected routes (authentication required)
+
 router.post('/add', authMiddleware, manufacturerController.addManufacturer);
 router.put('/update/:id', authMiddleware, manufacturerController.updateManufacturer);
 router.delete('/delete/:id', authMiddleware, manufacturerController.deleteManufacturer);
