f6f84c52ad0d322f319fcfd3607c86804ff02219
Ankit Tripathi <ankittripathi559@gmail.com>
2025-05-02 05:30:16 +0530

    TEST the backend API

--- server.js
+++ server.js
@@ -10,19 +10,23 @@ const Manufacturer = require('./models/Manufacturer');
 
 const app = express();
 
+
 app.use(cors());
 app.use(express.json());
 
+
 connectDB();
+
 
 app.use('/api', loginRoutes);
 app.use('/manufacturers', manufacturerRoutes);
+
 
 app.post('/add-manufacturer', verifyToken, async (req, res) => {
   const { name, category, city, products } = req.body;
 
   if (!name || !category || !city) {
-    return res.status(400).json({ message: 'Please fill all required fields' });
+    return res.status(400).json({ message: 'Please fill all reqired fields' });
   }
 
   try {
@@ -35,13 +39,28 @@ app.post('/add-manufacturer', verifyToken, async (req, res) => {
   }
 });
 
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Somthing went wrong!' });
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
+
+
+app.use('/api', loginRoutes);
+app.use('/manufacturers', manufacturerRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the SmartVyapaar Backend API');
+});
+
+
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
 });
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running at http:
-});
-
